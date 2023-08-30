b2c41db78219820bc537294326934a7034b0d484
EricZhou <neochau@gmail.com>
2023-08-30 18:21:39 +0800

    optimize hero2

--- components/hero.tsx
+++ components/hero.tsx
@@ -16,26 +16,18 @@ import {
   ResponsiveContainer,
   Tooltip,
 } from "recharts";
-import {
-  Menubar,
-  MenubarContent,
-  MenubarItem,
-  MenubarLabel,
-  MenubarMenu,
-  MenubarRadioGroup,
-  MenubarRadioItem,
-  MenubarSeparator,
-  MenubarShortcut,
-  MenubarSub,
-  MenubarSubContent,
-  MenubarSubTrigger,
-  MenubarTrigger,
-} from "@/components/ui/menubar";
+
 import { Button } from "@/components/ui/button";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { IconMinus, IconPlus, IconSpherePlus } from "@tabler/icons-react";
+import {
+  IconArticle,
+  IconLink,
+  IconMinus,
+  IconPlus,
+  IconSpherePlus,
+} from "@tabler/icons-react";
 import {
   ContextMenu,
   ContextMenuContent,
@@ -48,16 +40,6 @@ import {
 } from "@/components/ui/context-menu";
 import { cn } from "@/lib/utils";
 import { HTMLAttributes, useState, CSSProperties } from "react";
-
-type Playlist = (typeof playlists)[number];
-const playlists = ["Mick Jackson", "Acme Ltd"];
-
-interface Album {
-  name: string;
-  artist: string;
-  cover: string;
-}
-
 import qichun from "@/images/qichun.jpg";
 import wuhan from "@/images/wuhan.png";
 import hangzhou from "@/images/hangzhou.webp";
@@ -78,33 +60,11 @@ import {
   CardFooter,
 } from "@/components/ui/card";
 
-const placeItem: Album[] = [
-  {
-    name: "Qichun",
-    artist: "The village where I grew up",
-    cover: qichun.src,
-  },
-  {
-    name: "Wuhan",
-    artist: "The City where I studied",
-    cover: wuhan.src,
-  },
-  {
-    name: "Hangzhou",
-    artist: "The City where I worked",
-    cover: hangzhou.src,
-  },
-  {
-    name: "Zhuhai",
-    artist: "The city where my son likes",
-    cover: zhuhai2.src,
-  },
-  {
-    name: "Gree Coast",
-    artist: "The place My family lives",
-    cover: gelihaian.src,
-  },
-];
+interface Album {
+  name: string;
+  artist: string;
+  cover: string;
+}
 
 const madeForYouAlbums: Album[] = [
   {
@@ -135,11 +95,22 @@ const madeForYouAlbums: Album[] = [
   },
 ];
 
-interface SidebarProps extends HTMLAttributes<HTMLDivElement> {
-  playlists: Playlist[];
+interface Item {
+  name: string;
+  url: string;
+}
+function openNewTab(url: string) {
+  window.open(url, "_blank")?.focus();
 }
 
-function InnerSidebar({ className, playlists }: SidebarProps) {
+function InnerSidebar({ className }: HTMLAttributes<any>) {
+  const linkItems: Item[] = [
+    { name: "Github", url: "https://github.com/mojocn" },
+    { name: "Twitter", url: "https://twitter.com/neochau" },
+    { name: "Email", url: "mailto:neochau@gmail.com" },
+    { name: "Reddit", url: "https://reddit.com/trytv" },
+  ];
+
   return (
     <div className={cn("pb-12", className)}>
       <div className="space-y-4 py-4">
@@ -166,148 +137,35 @@ function InnerSidebar({ className, playlists }: SidebarProps) {
               </svg>
               Life Gallery
             </Button>
-            <Button variant="ghost" className="w-full justify-start">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mr-2 h-4 w-4"
-              >
-                <path d="M4.9 19.1C1 15.2 1 8.8 4.9 4.9" />
-                <path d="M7.8 16.2c-2.3-2.3-2.3-6.1 0-8.5" />
-                <circle cx="12" cy="12" r="2" />
-                <path d="M16.2 7.8c2.3 2.3 2.3 6.1 0 8.5" />
-                <path d="M19.1 4.9C23 8.8 23 15.1 19.1 19" />
-              </svg>
-              Radio
-            </Button>
-          </div>
-        </div>
-        <div className="px-3 py-2">
-          <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
-            Library
-          </h2>
-          <div className="space-y-1">
-            <Button variant="ghost" className="w-full justify-start">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mr-2 h-4 w-4"
-              >
-                <path d="M21 15V6" />
-                <path d="M18.5 18a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
-                <path d="M12 12H3" />
-                <path d="M16 6H3" />
-                <path d="M12 18H3" />
-              </svg>
-              Playlists
-            </Button>
-            <Button variant="ghost" className="w-full justify-start">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mr-2 h-4 w-4"
-              >
-                <circle cx="8" cy="18" r="4" />
-                <path d="M12 18V2l7 4" />
-              </svg>
-              Songs
-            </Button>
-            <Button variant="ghost" className="w-full justify-start">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mr-2 h-4 w-4"
-              >
-                <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-                <circle cx="12" cy="7" r="4" />
-              </svg>
-              Made for You
-            </Button>
-            <Button variant="ghost" className="w-full justify-start">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mr-2 h-4 w-4"
-              >
-                <path d="m12 8-9.04 9.06a2.82 2.82 0 1 0 3.98 3.98L16 12" />
-                <circle cx="17" cy="7" r="5" />
-              </svg>
-              Artists
-            </Button>
-            <Button variant="ghost" className="w-full justify-start">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mr-2 h-4 w-4"
-              >
-                <path d="m16 6 4 14" />
-                <path d="M12 6v14" />
-                <path d="M8 8v12" />
-                <path d="M4 4v16" />
-              </svg>
-              Albums
+            <Button
+              variant="ghost"
+              className="w-full justify-start"
+              onClick={(_) => {
+                openNewTab("https://mojotv.cn");
+              }}
+            >
+              <IconArticle size={14} className="mr-2" />
+              My Blog
             </Button>
           </div>
         </div>
         <div className="py-2">
           <h2 className="relative px-7 text-lg font-semibold tracking-tight">
-            Playlists
+            Social Links
           </h2>
           <ScrollArea className="h-[300px] px-1">
             <div className="space-y-1 p-2">
-              {playlists?.map((playlist, i) => (
+              {linkItems?.map((obj, i) => (
                 <Button
-                  key={`${playlist}-${i}`}
+                  key={`${obj.name}-${i}`}
                   variant="ghost"
+                  onClick={(e) => {
+                    openNewTab(obj.url);
+                  }}
                   className="w-full justify-start font-normal"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="mr-2 h-4 w-4"
-                  >
-                    <path d="M21 15V6" />
-                    <path d="M18.5 18a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
-                    <path d="M12 12H3" />
-                    <path d="M16 6H3" />
-                    <path d="M12 18H3" />
-                  </svg>
-                  {playlist}
+                  <IconLink size={14} className="mr-2" />
+                  {obj.name}
                 </Button>
               ))}
             </div>
@@ -315,206 +173,6 @@ function InnerSidebar({ className, playlists }: SidebarProps) {
         </div>
       </div>
     </div>
-  );
-}
-
-function InnerTopMusicNavBar() {
-  function gotoURL(url: string) {
-    //open url in a new tab
-    const win = window.open(url, "_blank");
-    win?.focus();
-  }
-
-  return (
-    <Menubar className="rounded-none border-b border-none px-2 lg:px-4">
-      <MenubarMenu>
-        <MenubarTrigger className="font-bold">Music</MenubarTrigger>
-        <MenubarContent>
-          <MenubarItem>About Music</MenubarItem>
-          <MenubarSeparator />
-          <MenubarItem>
-            Preferences... <MenubarShortcut>⌘,</MenubarShortcut>
-          </MenubarItem>
-          <MenubarSeparator />
-          <MenubarItem>
-            Hide Music... <MenubarShortcut>⌘H</MenubarShortcut>
-          </MenubarItem>
-          <MenubarItem>
-            Hide Others... <MenubarShortcut>⇧⌘H</MenubarShortcut>
-          </MenubarItem>
-          <MenubarShortcut />
-          <MenubarItem>
-            Quit Music <MenubarShortcut>⌘Q</MenubarShortcut>
-          </MenubarItem>
-        </MenubarContent>
-      </MenubarMenu>
-      <MenubarMenu>
-        <MenubarTrigger className="relative">File</MenubarTrigger>
-        <MenubarContent>
-          <MenubarSub>
-            <MenubarSubTrigger>New</MenubarSubTrigger>
-            <MenubarSubContent className="w-[230px]">
-              <MenubarItem>
-                Playlist <MenubarShortcut>⌘N</MenubarShortcut>
-              </MenubarItem>
-              <MenubarItem disabled>
-                Playlist from Selection <MenubarShortcut>⇧⌘N</MenubarShortcut>
-              </MenubarItem>
-              <MenubarItem>
-                Smart Playlist... <MenubarShortcut>⌥⌘N</MenubarShortcut>
-              </MenubarItem>
-              <MenubarItem>Playlist Folder</MenubarItem>
-              <MenubarItem disabled>Genius Playlist</MenubarItem>
-            </MenubarSubContent>
-          </MenubarSub>
-          <MenubarItem>
-            Open Stream URL... <MenubarShortcut>⌘U</MenubarShortcut>
-          </MenubarItem>
-          <MenubarItem>
-            Close Window <MenubarShortcut>⌘W</MenubarShortcut>
-          </MenubarItem>
-          <MenubarSeparator />
-          <MenubarSub>
-            <MenubarSubTrigger>Library</MenubarSubTrigger>
-            <MenubarSubContent>
-              <MenubarItem>Update Cloud Library</MenubarItem>
-              <MenubarItem>Update Genius</MenubarItem>
-              <MenubarSeparator />
-              <MenubarItem>Organize Library...</MenubarItem>
-              <MenubarItem>Export Library...</MenubarItem>
-              <MenubarSeparator />
-              <MenubarItem>Import Playlist...</MenubarItem>
-              <MenubarItem disabled>Export Playlist...</MenubarItem>
-              <MenubarItem>Show Duplicate Items</MenubarItem>
-              <MenubarSeparator />
-              <MenubarItem>Get Album Artwork</MenubarItem>
-              <MenubarItem disabled>Get Track Names</MenubarItem>
-            </MenubarSubContent>
-          </MenubarSub>
-          <MenubarItem>
-            Import... <MenubarShortcut>⌘O</MenubarShortcut>
-          </MenubarItem>
-          <MenubarItem disabled>Burn Playlist to Disc...</MenubarItem>
-          <MenubarSeparator />
-          <MenubarItem>
-            Show in Finder <MenubarShortcut>⇧⌘R</MenubarShortcut>{" "}
-          </MenubarItem>
-          <MenubarItem>Convert</MenubarItem>
-          <MenubarSeparator />
-          <MenubarItem>Page Setup...</MenubarItem>
-          <MenubarItem disabled>
-            Print... <MenubarShortcut>⌘P</MenubarShortcut>
-          </MenubarItem>
-        </MenubarContent>
-      </MenubarMenu>
-      <MenubarMenu>
-        <MenubarTrigger>Edit</MenubarTrigger>
-        <MenubarContent>
-          <MenubarItem disabled>
-            Undo <MenubarShortcut>⌘Z</MenubarShortcut>
-          </MenubarItem>
-          <MenubarItem disabled>
-            Redo <MenubarShortcut>⇧⌘Z</MenubarShortcut>
-          </MenubarItem>
-          <MenubarSeparator />
-          <MenubarItem disabled>
-            Cut <MenubarShortcut>⌘X</MenubarShortcut>
-          </MenubarItem>
-          <MenubarItem disabled>
-            Copy <MenubarShortcut>⌘C</MenubarShortcut>
-          </MenubarItem>
-          <MenubarItem disabled>
-            Paste <MenubarShortcut>⌘V</MenubarShortcut>
-          </MenubarItem>
-          <MenubarSeparator />
-          <MenubarItem>
-            Select All <MenubarShortcut>⌘A</MenubarShortcut>
-          </MenubarItem>
-          <MenubarItem disabled>
-            Deselect All <MenubarShortcut>⇧⌘A</MenubarShortcut>
-          </MenubarItem>
-          <MenubarSeparator />
-          <MenubarItem>
-            Smart Dictation...{" "}
-            <MenubarShortcut>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                className="h-4 w-4"
-                viewBox="0 0 24 24"
-              >
-                <path d="m12 8-9.04 9.06a2.82 2.82 0 1 0 3.98 3.98L16 12" />
-                <circle cx="17" cy="7" r="5" />
-              </svg>
-            </MenubarShortcut>
-          </MenubarItem>
-          <MenubarItem>
-            Emoji & Symbols{" "}
-            <MenubarShortcut>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                className="h-4 w-4"
-                viewBox="0 0 24 24"
-              >
-                <circle cx="12" cy="12" r="10" />
-                <path d="M2 12h20M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z" />
-              </svg>
-            </MenubarShortcut>
-          </MenubarItem>
-        </MenubarContent>
-      </MenubarMenu>
-
-      <MenubarMenu>
-        <MenubarTrigger className="hidden md:block">Account</MenubarTrigger>
-        <MenubarContent forceMount>
-          <MenubarLabel inset>Github Account</MenubarLabel>
-          <MenubarSeparator />
-          <MenubarRadioGroup value="mojocn">
-            <MenubarRadioItem
-              value="mojocn"
-              onClick={() => gotoURL("https://github.com/mojocn")}
-            >
-              mojocn
-            </MenubarRadioItem>
-            <MenubarRadioItem
-              value="go-gorm"
-              onClick={() => gotoURL("https://github.com/go-gorm")}
-            >
-              ORG:go-gorm
-            </MenubarRadioItem>
-            <MenubarRadioItem
-              value="bytegang"
-              onClick={() => gotoURL("https://github.com/bytegang")}
-            >
-              ORG:bytegang
-            </MenubarRadioItem>
-          </MenubarRadioGroup>
-          <MenubarSeparator />
-          <MenubarItem
-            inset
-            onClick={() => gotoURL("https://twitter.com/neochau")}
-          >
-            Twitter
-          </MenubarItem>
-          <MenubarSeparator />
-          <MenubarItem
-            inset
-            onClick={() => gotoURL("https://www.reddit.com/user/trytvorg")}
-          >
-            Reddit
-          </MenubarItem>
-        </MenubarContent>
-      </MenubarMenu>
-    </Menubar>
   );
 }
 
@@ -560,8 +218,8 @@ function InnerTabPersonal({
                 New Playlist
               </ContextMenuItem>
               <ContextMenuSeparator />
-              {playlists.map((playlist) => (
-                <ContextMenuItem key={playlist}>
+              {["awesome", "is", "me"].map((musicItem) => (
+                <ContextMenuItem key={musicItem}>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -574,7 +232,7 @@ function InnerTabPersonal({
                   >
                     <path d="M21 15V6M18.5 18a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5ZM12 12H3M16 6H3M12 18H3" />
                   </svg>
-                  {playlist}
+                  {musicItem}
                 </ContextMenuItem>
               ))}
             </ContextMenuSubContent>
@@ -1062,10 +720,38 @@ function InnerTabSkills() {
 }
 
 export default function Hero() {
+  const placeItem: Album[] = [
+    {
+      name: "Qichun",
+      artist: "The village where I grew up",
+      cover: qichun.src,
+    },
+    {
+      name: "Wuhan",
+      artist: "The City where I studied",
+      cover: wuhan.src,
+    },
+    {
+      name: "Hangzhou",
+      artist: "The City where I worked",
+      cover: hangzhou.src,
+    },
+    {
+      name: "Zhuhai",
+      artist: "The city where my son likes",
+      cover: zhuhai2.src,
+    },
+    {
+      name: "Gree Coast",
+      artist: "The place My family lives",
+      cover: gelihaian.src,
+    },
+  ];
+
   return (
     <div className="bg-background">
       <div className="grid lg:grid-cols-5">
-        <InnerSidebar playlists={playlists} className="hidden lg:block" />
+        <InnerSidebar className="hidden lg:block" />
         <div className="col-span-3 lg:col-span-4 lg:border-l">
           <div className="h-full px-4 py-6 lg:px-8">
             <Tabs defaultValue="music" className="h-full space-y-6">
